97e8fdd81415d0a38ac9bdbafb831ad124c2eb05
vividwhisp <korivinesh@gmail.com>
2026-02-02 18:44:52 +0530

    Started the development

--- chess.py
+++ chess.py
@@ -1,5 +1,13 @@
 import pygame
+import sys
 
 
+# Initialising pygame module
 pygame.init()
 
+# Setting Width and height of the Chess Game screen
+WIDTH = 1000
+HEIGHT = 900
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Chess Game')
